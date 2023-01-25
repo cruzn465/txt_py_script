1b1deedf6683449774c022cf336ce9900ea67f78
Nichole <nichole.cruzalegui@bbdo.com>
2023-01-25 13:22:42 -0500

    Finishes adding to an array and then dumping into json

--- txt2csv.py
+++ txt2csv.py
@@ -1,6 +1,7 @@
 import glob
 import csv
 import json
+from pickle import DICT
 from tkinter.ttk import Separator
 
 
@@ -8,76 +9,76 @@ from tkinter.ttk import Separator
 read_files = glob.glob('output/*')
 arr = []
 
-#with open("HD_SpringRadio.csv", mode="w", encoding="utf-8") as outfile:
+# with open("HD_SpringRadio.csv", mode="w", encoding="utf-8") as outfile:
 
-    #w = csv.writer(outfile)
+#w = csv.writer(outfile)
 count = 0
 for f in read_files:
     if count < 1:
+
         infile = open(f, 'r')
         data = infile.read()
+
+        # A list of each "line" and use this for step 2 of parsing the string
+        lines = data.splitlines(False)
+
+        # print(lines)
 
         # FXN that takes the data from each file and adds it to the global array
         def addToArr():
             dict = {}
-            seps = ["CLIENT:", "JOB:", "TITLE:", "JOB#",
-                    "SLATE #:", "DATE:", "ANNCR:", "LEGAL:"]
-            seps_i = 0
-            string = str(data)
 
-            while seps_i < len(seps)-1:
-                # separate the line and add it to dict
-                line = string.split(seps[seps_i+1])[0]
-                val_arr = line.split(seps[seps_i])
-                dict[seps[seps_i]] = val_arr[1]
+            # i.e.['', '\tTHE HOME DEPOT']
+            # Make reusable code and find the line in the list that starts with "CLIENT"
+            clientVal = lines[0].split("CLIENT:")[1]
+            dict["Client"] = clientVal
 
-                temp_str = string.split(line)[1]
-                string = temp_str
+            jobVal = lines[1].split("JOB:")[1]
+            dict["Job"] = jobVal
 
-                # print(val_arr[1])
-                # print("*******")
-                seps_i += 1
-
-            # LEGAL
-            # print(string)
-            val_arr = string.split("LEGAL:")
-            dict[seps[seps_i]] = val_arr[1]
+            titleVal = lines[2].split("TITLE:")[1]
+            dict["Title"] = titleVal
             # print(dict)
 
-            print(dict["ANNCR:"])
-            # client_val = client.split("CLIENT:")
-            # dict["CLIENT:"] = client_val[1]
+            arr.append(dict)
+            print(arr)
 
-            # separate "CLIENT: THE HOME DEPOT" and add it to dict
-            # client = string.split("JOB:")[0]
-            # client_val = client.split("CLIENT:")
-            # dict["CLIENT:"] = client_val[1]
+            # seps = ["CLIENT:", "JOB:", "TITLE:", "JOB#",
+            #         "SLATE #:", "DATE:", "ANNCR:", "LEGAL:"]
+            # seps_i = 0
+            # string = str(data)
 
-            # remove the first sep
-            # temp_str = string.split(client)[1]
-            # string = temp_str
+            # # Separates lines and removes from string
+            # while seps_i < len(seps)-1:
 
-            # REPEAT FOR "JOB:    :30 L&G Divisional Radio"
-            # job = string.split("TITLE:")[0]
-            # job_val = job.split("JOB:")
-            # dict["JOB:"] = job_val[1]
+            #     # line: everything before 2nd separator i.e. 1st loop is "JOB:"
+            #     # 1st line: "CLIENT:\tTHE HOME DEPOT\n"
 
-            # temp_str = string.split(job)[1]
-            # string = temp_str
+            #     # 1st val_arr: ['', '\tTHE HOME DEPOT\n']
+            #     line = string.split(seps[seps_i+1])[0]
+            #     val_arr = line.split(seps[seps_i])
 
-            # print(job)
-            # print(job_val)
-            # print(dict)
-            # print(string)
+            #     dict[seps[seps_i]] = val_arr[1]
 
-        # invoke addToArr fxn
-            with open("output.json", "w") as outfile:
-                json.dump(dict, outfile)
+            #     temp_str = string.split(line)[1]
+            #     string = temp_str
 
+            #     seps_i += 1
+
+            # # LEGAL
+            # val_arr = string.split("LEGAL:")
+            # dict[seps[seps_i]] = val_arr[1]
         addToArr()
+
+        # with open("output.json", "w") as outfile:
+        #     json.dump(arr, outfile)
 
         infile.close()
     count += 1
+
+# At the end of looping thru read_files, dump into output.json
+with open("output.json", "w") as outfile:
+    json.dump(arr, outfile)
 
 # with open(f, "r") as infile:
 # #     if count < 1:
@@ -86,5 +87,3 @@ for f in read_files:
 # #         print(infile)
 
 #     w.writerow([line for line in infile])
-
-
