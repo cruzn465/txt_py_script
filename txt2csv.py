d4a18329264aebeb56d0197327d86adcbd9589e9
Nichole <nichole.cruzalegui@bbdo.com>
2023-02-03 13:07:25 -0500

    Eric edits

--- txt2csv.py
+++ txt2csv.py
@@ -1,3 +1,4 @@
+import jsonlines
 import glob
 import csv
 import json
@@ -6,7 +7,9 @@ from tkinter.ttk import Separator
 
 
 # note YHTB-D28R-5056.txt " CLIENT:	THE HOME DEPOT"
-read_files = glob.glob('output/*')
+# read_files = glob.glob('output/*')
+read_files = glob.glob('outliers/*')
+
 arr = []
 # NEEDS BETTER NAMING CONVENTIONS
 # each_str = ["CLIENT", "JOB", "TITLE"]
@@ -19,14 +22,18 @@ comp_str = ["ANNCR", "ANNC", "VO", "ALT", "ED"]
 # w = csv.writer(outfile)
 count = 0
 for f in read_files:
-    if count < 400:
+    if count < 100:
         # print("*********COUNT: ", count)
         infile = open(f, 'r')
+        # print(infile)
         data = infile.read()
 
         # A list of each "line" and use this for step 2 of parsing the string
         lines = data.splitlines(False)
-        filt_list = list(filter(lambda str: str != "", lines))
+        filt_list1 = list(filter(lambda str: str != "", lines))
+        filt_list = list(filter(lambda str: str != " ", filt_list1))
+
+        print("FILT_LIST", filt_list)
 
         # FXN that takes the data from each file and adds it to the global array
         def addToArr():
@@ -37,33 +44,43 @@ for f in read_files:
                 for j in range(len(filt_list)):
                     if filt_list[j].find(each_str[i]) != -1:
                         val = filt_list[j].split(each_str[i])[1]
-                        dict["prompt"] = val.replace(
-                            ":", "").replace("\t", "")+"\n\n###\n\n"
+                        # print(val)
+                        dict["prompt"] = ""
+                        # dict["prompt"] = val.replace(":", "").replace("\t", "")+"\n\n###\n\n"
                         break
 
             # COUNT IS THE AMOUNT OF TIMES A COMP_STR APPEARS
-            count = 0
+            comp_count = 0
             curr_comp = ""
-            while len(filt_list) > 0 and count < 1:
+            while len(filt_list) > 0 and comp_count < 1:
                 # FOR EACH FILT_LINE, LOOP THROUGH COMP_STR
                 for i in range(len(comp_str)):
                     # IF THERE IS A COMP_STR, ADD TO COUNT TO BREAK OUT OF OUTER LOOP
                     if filt_list[0].find(comp_str[i]) != -1:
+                        # print(filt_list[0])
                         curr_comp = comp_str[i]
-                        count += 1
+                        comp_count += 1
                         break
                 # POP THE LINE IF THERE ARE NO COMP_STR PRESENT
-                if count == 0:
+                if comp_count == 0:
                     filt_list.pop(0)
 
+            # print("after while loop", filt_list)
+
             # DELETE LEGAL
-            last_i = len(filt_list)-1
+            idx_legal = 0
             if len(filt_list) != 0:
-                if filt_list[last_i].find("LEGAL") != -1:
-                    filt_list.pop(last_i)
+                # INSTEAD OF JUST CHECKING THE LAST INDEX, LOOP THROUGH THE FILT_LIST AND FIND "LEGAL", SAVE THE IDX AND DELETE IT AFTER THE LOOP IS OVER
+                for k in range(len(filt_list)):
+                    if filt_list[k].find("LEGAL") != -1:
+                        idx_legal = k
+                        break
+                if idx_legal != 0:
+                    filt_list.pop(idx_legal)
 
                 # JOIN THE FILT_LIST, SEPARATE THE CURR_COMP AND ADD TO DICT UNDER COMPLETION
-                joined_str = "".join(filt_list)
+                joined_str = " ".join(filt_list)
+                # print(joined_str)
                 val_str = joined_str.split(curr_comp)[1]
 
                 if val_str.find(":") != -1:
@@ -84,8 +101,16 @@ for f in read_files:
     count += 1
 
 # At the end of looping thru read_files, dump into output.json
-with open("output.json", "w") as outfile:
-    json.dump(arr, outfile)
+# with open("output.json", "w") as outfile:
+#     json.dump(arr, outfile)
+
+# write to file
+with jsonlines.open('sample2_op2.jsonl', 'w') as outfile:
+    outfile.write_all(arr)
+# read the file
+# with jsonlines.open('sample2_op2.jsonl') as reader:
+#     for obj in reader:
+#         print(obj)
 
 # with open(f, "r") as infile:
 # #     if count < 1:
